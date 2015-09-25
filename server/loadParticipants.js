0fe2ce2fad4a5340bd84e4b00b5139966470f717
Johan Kardell <johan.kardell@primalogic.org>
2015-09-25 13:54:48 +0200

    updated model

--- server/loadParticipants.js
+++ server/loadParticipants.js
@@ -1,7 +1,7 @@
 Meteor.startup(function () {
 
   // seed position
-  if(Participants.find().count === 0)
+  if(Participants.find().count() === 0)
   {
     Participants.insert({
       _id : 1,
@@ -10,10 +10,10 @@ Meteor.startup(function () {
     
     ParticipantPositions.insert({
       id: 0,
-      participant_id: "1",
+      participantId: "1",
       latitude : "65",
       longitude : "24",
-      timestamp : "20150313T14:17:19",
+      timeStamp : "20150313T14:17:19",
       alarm : false
     });
   }
