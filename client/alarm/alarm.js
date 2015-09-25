53cdbbf75f784c894a81e6da6e330c303c785d43
martingreer <greer@student.chalmers.se>
2015-09-25 17:13:07 +0100

    Created function for changing alarm status and fixed a bug.

--- client/alarm/alarm.js
+++ client/alarm/alarm.js
@@ -1,23 +1,29 @@
 var isAlarm = false;
 
 Template.alarm.rendered = function(){
-    isAlarm = Session.get("isAlarmed");
+    setAlarm(Session.get("isAlarmed"));
 };
 
 Template.alarm.events({
     'click #btnAlarm': function () {
         if (isAlarm) {
-            isAlarm = false;
-            Session.set("isAlarmed", false);
-            $('#btnAlarm').removeClass("alarmed");
-            SetLocation();
+            setAlarm(false);
         } else {
-            isAlarm = true;
-            Session.set("isAlarmed", true);
-            $('#btnAlarm').addClass("alarmed");
-            SetLocation();
+            setAlarm(true);
         }
 
         console.log("Alarm status:" + isAlarm);
     }
 });
+
+var setAlarm  = function(isAlarmed){
+    isAlarm = isAlarmed;
+    Session.set("isAlarmed", isAlarmed);
+    if(isAlarmed){
+        $('#btnAlarm').addClass("alarmed");
+    }
+    else{
+        $('#btnAlarm').removeClass("alarmed");
+    }
+    SetLocation();
+};
