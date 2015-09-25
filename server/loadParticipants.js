e79dc66750a2ce69c3786e3568675902aa28e004
unknown <erb@erb.softhouse.se>
2015-09-25 15:56:57 +0200

    merge distance calc

--- server/loadParticipants.js
+++ server/loadParticipants.js
@@ -1,5 +1,4 @@
 Meteor.startup(function () {
-
     Participants.insert({
       name : 'Mr Andersson'
     });
@@ -11,7 +10,6 @@ Meteor.startup(function () {
       timeStamp : "1443184906",
       alarm : false
     });
-
 
     // Following code is just for demo purpose
     function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
@@ -34,6 +32,4 @@ Meteor.startup(function () {
 
     var testDistance = getDistanceFromLatLonInKm(57.70887,11.97456,57.7004325,11.95170879);
     console.log("Distance from Centralen to Järntorget: " + testDistance + " km");
-
-
 });
