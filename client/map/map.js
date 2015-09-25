c6e88cd058a0d80c02a1311fe762062d70aa3acc
unknown <ton@TON.softhouse.se>
2015-09-25 16:51:15 +0200

    continued the map works

--- client/map/map.js
+++ client/map/map.js
@@ -1,7 +1,7 @@
 /*
 
 */
-
+var markers = {};
 
 Template.map.helpers({
   exampleMapOptions: function() {
@@ -20,40 +20,68 @@ Template.map.helpers({
 
 Template.map.onCreated(function() {
   // We can use the `ready` callback to interact with the map API once the map is ready.
+  
+//var image = 'images/ALARM.png';
   GoogleMaps.ready('exampleMap', function(map) {
-console.log("Starting");
+		console.log("Starting");
+
+		/*
     	ParticipantPositions.find().forEach(function(document){
     		console.log(document);
     		//console.log("latitude: " + document.latitude);
     		//console.log("longitude: " + document.longitude);
 
 	    	var marker = new google.maps.Marker({
-	      		draggable: true,
+	      		draggable: false,
 	      		animation: google.maps.Animation.DROP,
 	      		position: new google.maps.LatLng(document.latitude, document.longitude),
 	      		map: map.instance,
+	      		icon: image,
 		      // We store the document _id on the marker in order 
 		      // to update the document within the 'dragend' event below.
 	      		id: document._id
     		});
 
     	})
+*/
 
 
-var markers = {};
 
-Participants.find().observe({  
+ParticipantPositions.find().observe({  
   added: function(document) {
+	
+
+//var image = 'images/ALARM.jpg';
+
+  	console.log("Added new marker");
+  	console.log(document);
     // Create a marker for this document
     var marker = new google.maps.Marker({
-      draggable: true,
+      draggable: false,
       animation: google.maps.Animation.DROP,
       position: new google.maps.LatLng(document.latitude, document.longitude),
       map: map.instance,
+      label: "N",
+      //icon: image,
       // We store the document _id on the marker in order 
       // to update the document within the 'dragend' event below.
       id: document._id
     });
+
+/*
+if(Status == ALARM){
+	
+}
+else if(Status == POTENTIAL_ALARM){
+	
+}
+else{
+	
+}
+*/
+
+
+
 
     // This listener lets us drag markers on the map and update their corresponding document.
  //   google.maps.event.addListener(marker, 'dragend', function(event) {
@@ -64,9 +92,13 @@ Participants.find().observe({
     markers[document._id] = marker;
   },
   changed: function(newDocument, oldDocument) {
+  	console.log("changed marker");
+  	console.log(newDocuments);
     markers[newDocument._id].setPosition({ lat: newDocument.latitude, lng: newDocument.longitude });
   },
   removed: function(oldDocument) {
+  	console.log("deleted marker");
+  	console.log(oldDocument);
     // Remove the marker from the map
     markers[oldDocument._id].setMap(null);
 
