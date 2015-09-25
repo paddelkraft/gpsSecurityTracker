bd1d20f22bbad7aebfd7fffe7c5970ad32592e19
martingreer <greer@student.chalmers.se>
2015-09-25 17:06:21 +0100

    Alarm code updated.

--- client/alarm/alarm.js
+++ client/alarm/alarm.js
@@ -1,18 +1,21 @@
 var isAlarm = false;
-/*
-Template.alarmed.rendered = function(){
-    Session.get(isAlarmed);
-};*/
+
+Template.alarm.rendered = function(){
+    isAlarm = Session.get("isAlarmed");
+};
 
 Template.alarm.events({
     'click #btnAlarm': function () {
         if (isAlarm) {
             isAlarm = false;
-            Session.set("isAlarmed", isAlarm);
+            Session.set("isAlarmed", false);
             $('#btnAlarm').removeClass("alarmed");
+            SetLocation();
         } else {
             isAlarm = true;
+            Session.set("isAlarmed", true);
             $('#btnAlarm').addClass("alarmed");
+            SetLocation();
         }
 
         console.log("Alarm status:" + isAlarm);
