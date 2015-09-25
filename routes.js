bf7c8e6e93417f958daf74797d92b11db0d7d4e7
unknown <ton@TON.softhouse.se>
2015-09-25 14:12:19 +0200

    fixed routing for the map-html and added needed package for google  maps api

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 Router.route('/');
 Router.route('/home');
-Router.route('/viewparticipants')
+Router.route('/viewparticipants');
+Router.route('/map');
 
 Router.configure({
     layoutTemplate: 'main'
